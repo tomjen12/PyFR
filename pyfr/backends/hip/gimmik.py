944ad4c38294c20b06dc920606afc91dc0045579
tomjen12 <tomjen12@amd.com>
2026-07-24 00:18:08 -0500

    Use precomputed GiMMiK launch metadata

--- pyfr/backends/hip/gimmik.py
+++ pyfr/backends/hip/gimmik.py
@@ -199,11 +199,7 @@ class HIPGiMMiKKernels(HIPKernelProvider):
                 detail['build_kernel_s'] += time.perf_counter() - tbuild
                 detail['candidate_count'] += 1
 
-                grid_y = meta.get('grid_y', 1)
-                ncolsv = (
-                    meta.get('width', 1) *
-                    meta.get('ncols', meta['block'][0])
-                )
+                grid_y, ncolsv = meta['grid_y'], meta['ncolsv']
                 grid = (-(-n // ncolsv), grid_y, 1)
                 params = kern.make_params(grid, meta['block'])
                 params.set_args(n, b, ldb, out, ldc)
